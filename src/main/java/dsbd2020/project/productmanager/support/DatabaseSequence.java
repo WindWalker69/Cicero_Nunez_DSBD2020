3a432fcae1053a14d9bdc3e7e8ca2ea1a1fc49a9
WindWalker69 <57485129+WindWalker69@users.noreply.github.com>
2020-12-26 16:38:25 +0100

    commit

---
+++ src/main/java/dsbd2020/project/productmanager/support/DatabaseSequence.java
@@ -0,0 +1,28 @@
+package dsbd2020.project.productmanager.support;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "database_sequences")
+public class DatabaseSequence {
+
+    @Id
+    private String id;
+    private Integer seq;
+
+    public String getId() {
+        return id;
+    }
+    public DatabaseSequence setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public Integer getSeq() {
+        return seq;
+    }
+    public DatabaseSequence setSeq(Integer seq) {
+        this.seq = seq;
+        return this;
+    }
+}
